af812f498b6ef90bce19efb3e6b2db0876dd796c
Arvind Prashanth <55134368+arviinddd@users.noreply.github.com>
2024-06-26 23:32:41 -0400

    Add files via upload

---
+++ mybot/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ChatWindow from './components/ChatWindow'; // Ensure this path is correct
+import './App.css'; // Ensure this path is correct
+
+function App() {
+    return (
+        <div className="App">
+            <ChatWindow />
+        </div>
+    );
+}
+
+export default App;
+
+
